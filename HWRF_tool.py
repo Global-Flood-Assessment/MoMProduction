dec4552bfbbe98aee961723910aac4a8a75291f1
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2021-12-05 12:15:03 -0500

    HWRF: tiff

--- HWRF_tool.py
+++ HWRF_tool.py
@@ -65,6 +65,82 @@ def HWRF_download(hwrfurl):
     
     return ascii_list
 
+def process_rain(adate,TC_Rain):
+    """process rainfall data"""
+
+    ## VRT template to read the csv
+    vrt_template="""<OGRVRTDataSource>
+        <OGRVRTLayer name='{}'>
+            <SrcDataSource>{}</SrcDataSource>
+            <GeometryType>wkbPoint</GeometryType>
+            <GeometryField encoding="PointFromColumns" x="lon" y="lat" z="Z"/>
+        </OGRVRTLayer>
+    </OGRVRTDataSource>"""
+
+    ## Read each text file and create the separate tiff file
+    for i in TC_Rain:
+        with open(i,'r') as f:
+            variable=csv.reader(f, delimiter=' ') 
+            row_count=1 
+            for row in variable:
+                if row_count == 1: 
+                    while ('' in row):
+                        row.remove('')
+                    XLC=float(row[0]) 
+                    XRC=float(row[1]) 
+                    YBC=float(row[2]) 
+                    YTC=float(row[3])
+                    res=float(row[4])
+                    nrows=float(row[5])
+                    ncol=float(row[6])
+                    row_count = row_count + 1
+        df = (pd.read_table(i, skiprows=1, delim_whitespace=True, names=('lat', 'lon', 'Z'))).fillna(-999)
+        df.sort_values(by=["lat","lon"], ascending=[False, True])
+        df=df[['lon','lat','Z']]
+        df = df[df.lon >= XLC]
+        df = df[df.lon <= XRC]
+        df = df[df.lat >= YBC]
+        df = df[df.lat <= YTC]
+        df = df[df.Z > 0]
+        df.to_csv(i.replace(".ascii",".csv"),index=False, sep=" ")
+        with open(i.replace(".ascii",".vrt"),"w") as g:
+            g.write(vrt_template.format(i.replace(".ascii",""),i.replace(".ascii",".csv")))
+        g.close()
+        r=gdal.Rasterize(i.replace(".ascii",".tiff"),i.replace(".ascii",".vrt"),outputSRS="EPSG:4326",xRes=res, yRes=res,attribute="Z", noData=-999)
+        r=None
+        os.remove(i.replace(".ascii",".csv"))
+
+    ##merge all  tiffs file als
+    # nd delete the individual tiff, vrt and ascii file 
+
+    TC_Rain_tiff=[]
+    for i in TC_Rain:
+        TC_Rain_tiff.append(i.replace(".ascii",".tiff"))
+
+    filename="hwrf."+ adate +"rainfall.vrt"
+    raintiff = filename.replace(".vrt",".tiff")
+    vrt = gdal.BuildVRT(filename, TC_Rain_tiff)
+    gdal.Translate(raintiff, vrt)
+    vrt=None
+    
+    # no need
+    #gdalcmd = "gdal_translate -of GTiff " + filename + " " + raintiff
+    #subprocess.call(gdalcmd, shell=True)
+
+    # create a zipfile
+    zip_file="hwrf."+ adate +"rainfall.zip"
+    with zipfile.ZipFile(zip_file, 'w',zipfile.ZIP_DEFLATED) as zipObj:
+        for i in TC_Rain_tiff:
+            asfile = i.replace(".tiff",".ascii")
+            zipObj.write(asfile)
+    
+    for i in TC_Rain_tiff:
+        os.remove(i)
+        os.remove(i.replace(".tiff",".ascii"))
+        os.remove(i.replace(".tiff",".vrt"))
+
+    return raintiff
+
 def HWRF_cron():
     """ main cron script"""
     
@@ -83,13 +159,12 @@ def HWRF_cron():
     for key in datelist:
         logging.info("check: " + key)
         a_list = HWRF_download(datelist[key])
-        print(a_list)
         if len(a_list) == 0:
             logging.info("no rainfall data " + key)
             continue
         logging.info("processing " + key)
-        #newtiff = process_rain(key,a_list)
-        #newtiff = "hwrf."+ key +"rainfall.tiff"
+        newtiff = process_rain(key,a_list)
+        print(newtiff)
         #logging.info("processing " + newtiff)
         #[hwrfcsv,dataflag] = HWRF_extract_by_watershed(newtiff)
         # if not dataflag:
