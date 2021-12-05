dc5c54db9d930e2659219a05ec5f9e7c1bf52c44
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2021-12-05 12:09:54 -0500

    HWRF: test download ...

--- HWRF_tool.py
+++ HWRF_tool.py
@@ -48,12 +48,64 @@ def generate_procesing_list():
     
     return datelist
 
+def HWRF_download(hwrfurl):
+    """ download rainfall data"""
+    reqs = requests.get(hwrfurl)
+    soup = BeautifulSoup(reqs.text,"html.parser")
+
+    ascii_list = []
+    for link in soup.find_all('a'):
+        fstr = link.string
+        if "rainfall.ascii" in fstr:
+            fstr_local = os.path.join(settings.HWRF_PROC_DIR, fstr)
+            if not os.path.exists(fstr_local):
+                wgetcmd = "wget " + os.path.join(hwrfurl,fstr) + " -P " + settings.HWRF_PROC_DIR
+                subprocess.call(wgetcmd, shell=True)
+            ascii_list.append(fstr)
+    
+    return ascii_list
+
 def HWRF_cron():
     """ main cron script"""
     
     # get date list
     datelist = generate_procesing_list()
-    print(datelist)
+    #print(datelist)
+
+    if len(datelist) == 0:
+        logging.info("no new data to process!")
+        sys.exit(0)
+    
+    # switch to processing folder
+    os.chdir(settings.HWRF_PROC_DIR)
+
+    # download - process ascii
+    for key in datelist:
+        logging.info("check: " + key)
+        a_list = HWRF_download(datelist[key])
+        print(a_list)
+        if len(a_list) == 0:
+            logging.info("no rainfall data " + key)
+            continue
+        logging.info("processing " + key)
+        #newtiff = process_rain(key,a_list)
+        #newtiff = "hwrf."+ key +"rainfall.tiff"
+        #logging.info("processing " + newtiff)
+        #[hwrfcsv,dataflag] = HWRF_extract_by_watershed(newtiff)
+        # if not dataflag:
+        #     logging.info("no data: " + hwrfcsv)
+        #     continue
+        #logging.info("generated: " + hwrfcsv)
+        #os.chdir(scriptdir)
+        
+        # run MoM update
+        #testdate = key
+        #update_HWRF_MoM(testdate,GFMS,GLoFAS,HWRFsummary,HWRFmom)
+
+
+    os.chdir(settings.BASE_DIR)
+
+    return
 
 def main():
     HWRF_cron()
