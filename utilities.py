1d34604e73fb5391ab638f94218307db9a8e2c7f
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2022-04-03 21:19:34 -0400

    add from today function

--- utilities.py
+++ utilities.py
@@ -7,6 +7,7 @@ routines shared across the modules
 import settings
 import pandas as pd
 import geopandas 
+from datetime import date,timedelta,datetime
 
 def watersheds_gdb_reader():
     """reader watersheds gdb into geopandas"""
@@ -25,15 +26,40 @@ def watersheds_gdb_reader():
 
 def read_data(datafile):
     df = pd.read_csv(datafile)
-    df = pd.DataFrame(df)
+    #df = pd.DataFrame(df)
     return df
 
+def from_today(adate):
+    """caculate days between adate (in YYYYMMDD) and today"""
+
+    # conver adate to date object
+    # adate may come in as YYYYMMDD
+    da = datetime.strptime(adate[:8],"%Y%m%d").date()
+    today = date.today()
+    delta = da - today
+
+    return delta.days
+    
 
 def main():
     ''' test routines'''
 
     watershed = watersheds_gdb_reader()
     print(watershed.head)
+
+    # test from today function
+    adate = date.today().strftime("%Y%m%d")
+    ddays = from_today(adate)
+    print("{} => {}".format(adate,ddays))
+
+    yesterday = date.today() - timedelta(days = 2)
+    adate = yesterday.strftime("%Y%m%d")
+    ddays = from_today(adate)
+    print("{} => {}".format(adate,ddays))
+
+    adate = adate + "18"
+    ddays = from_today(adate)
+    print("{} => {}".format(adate,ddays))
 
 if __name__ == '__main__':
     main()
